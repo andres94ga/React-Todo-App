60b6626c05fbda12e5f628df22f6015f53b53b7f
AndresGaona94 <andy_gaona09@hotmail.com>
2025-06-23 15:05:36 -0300

    Index error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import "./index.css";
 import Nav from "./components/Nav/Nav";
-import Index from "./components/Page";
+import Index from "./components/Page/PageIndex";
 
 function App() {
   const [count, setCount] = useState(0);
